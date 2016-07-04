17978c75c75732d5f4b2a9925913060cb66866b4
Justin Beuck <jbeuck@yahoo.com>
2016-07-04 07:28:23 -0500

    Accessing Dispatch and State with Redux

--- client/store.js
+++ client/store.js
@@ -1,19 +1,19 @@
- import { syncHistoryWithStore } from 'react-router-redux';
-  import { browserHistory } from 'react-router';
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
   
- import rootReducer from './reducers/index'; 
- import rootReducer from './reducers/index';
+import rootReducer from './reducers/index'; 
  
- import comments from './data/comments';
- import posts from './data/posts';
+import comments from './data/comments';
+import posts from './data/posts';
  
- const defaultState = {
-   posts,
-   comments
- }
+const defaultState = {
+  posts,
+  comments
+}
+
+const store = createStore(rootReducer, defaultState);
  
- const store = createStore(rootReducer, defaultState);
- 
- export const history = syncHistoryWithStore(browserHistory, store);
- 
- export default store;
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
