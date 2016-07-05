cd483ec5f299bbc45ec0440d7c1e2bbafb76c88e
Justin Beuck <jbeuck@yahoo.com>
2016-07-05 06:44:36 -0500

    adding reducers to change the state

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,3 +1,4 @@
+// increment
 export function increment(index) {
   return {
     type: 'INCREMENT_LIKES',
@@ -5,19 +6,22 @@ export function increment(index) {
   }
 }
 
+// add comment
 export function addComment(postId, author, comment) {
   return {
-    type: ADD_COMMENT,
+    type: 'ADD_COMMENT',
     postId,
     author,
     comment
   }
 }
 
+// remove comment
+
 export function removeComment(postId, i) {
   return {
-    type: REMOVE_COMMENT,
-    postId,
-    i
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
   }
 }
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,18 +1,19 @@
+
 import React from 'react';
 
-import { render } from 'react-dom'
+import { render } from 'react-dom';
 
 // Import css
 import css from './styles/style.styl';
 
+// Import Components
 import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
+// import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
 import { Provider } from 'react-redux';
-
 import store, { history } from './store';
 
 const router = (
@@ -20,10 +21,10 @@ const router = (
     <Router history={history}>
       <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
+        <Route path="/view/:postId" component={Single}></Route>
       </Route>
     </Router>
   </Provider>
-  )
+)
 
 render(router, document.getElementById('root'));
