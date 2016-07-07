84cd388f57ab698ff00d2dd2f652876d42542ff8
Justin Beuck <jbeuck@yahoo.com>
2016-07-06 20:58:58 -0500

    Redux reducer composition

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,34 @@
+function postComments(state = [], action) {
+	switch(action.type) {
+		case 'ADD_COMMENT':
+		return [...state,{
+			user: action.author,
+			text: action.comment
+		}];
+		case 'REMOVE_COMMENT':
+		// return new state without deleted comment
+		return [
+		//from the start to the one we want to delete
+			...state.slice(0, action.i),
+			...state.slice(action.i + 1)
+		]
+		default: 
+			return state;
+
+	}
+	return state;
+}
+
 
 function comments(state = [], action) {
+	if(typeof action.postId !== 'undefined') {
+		// take the current state
+		return {
+			...state,
+			// overwrite this post with a new one
+			[action.postId]: postComments(state[action.postId], action)
+		}
+	}
   return state;
 }
 
